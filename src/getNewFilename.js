12a76cbad0dd9d688e0c6de5232272f9622b2f6b
Alex Lin <allienx@hashmail.cc>
2021-11-07 21:11:03 -0800

    Add support for renaming organized photos

--- src/getNewFilename.js
+++ src/getNewFilename.js
@@ -1,3 +1,11 @@
+import path from 'path'
+
+import last from 'lodash/last.js'
+
 export default function getNewFilename({ index, filePath }) {
-  return filePath
+  const {dir, name, ext} = path.parse(filePath)
+
+  const newName = last(name.split('_'))
+
+  return path.join(dir, `${newName}${ext}`)
 }
