330b291aa6657e8d45f877dac623aaf88e76ba8d
Alex Lin <allienx@hashmail.cc>
2022-02-23 22:21:57 -0800

    Start index at 1

--- src/getNewFilename.js
+++ src/getNewFilename.js
@@ -3,7 +3,7 @@ import path from 'path'
 export default function getNewFilename({ index, prefix, filePath }) {
   const { dir, ext } = path.parse(filePath)
 
-  const newName = `${prefix}-${index.toString().padStart(4, '0')}`
+  const newName = `${prefix}-${(index + 1).toString().padStart(4, '0')}`
 
   return path.join(dir, `${newName}${ext}`.toLowerCase())
 }
