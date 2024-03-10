ca334881cca77ae373464db5aa35f26019deedf2
Alex Lin <allienx@hashmail.cc>
2024-03-10 14:27:16 -0700

    Bump prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,11 @@
-module.exports = {
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
   semi: false,
   singleQuote: true,
+  tabWidth: 2,
   trailingComma: 'all',
 }
+
+module.exports = config
